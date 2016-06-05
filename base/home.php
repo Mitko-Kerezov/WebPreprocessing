a952eb0262c9156a4439171bb6ed4b68175b13eb
Dimitar Kerezov <dimitar.kerezov@telerik.com>
2016-06-05 15:40:28 +0300

    Sell me this pen

--- base/home.php
+++ base/home.php
@@ -3,7 +3,7 @@ require_once('db_communication/auth.php');
 $title = 'Welcome - '.$user_row['email'];
 require_once('layout/header.php');
 ?>
-	<h1>Heading</h1>
+	<h1>Welcome to preprocessing, where things are optimized and data is already available @ runtime</h1>
 <?php
 require_once('layout/footer.php');
 ?>
